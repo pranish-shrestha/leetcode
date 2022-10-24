2f3fcc188ca290a97da2f561502de2e377293d17
spranish45@gmail.com <spranish45@gmail.com>
2022-10-24 20:32:29 +0545

    palindrome_number

---
+++ 9.py
@@ -0,0 +1,9 @@
+'''
+9. Palindrome Number
+https://leetcode.com/problems/palindrome-number/
+'''
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
