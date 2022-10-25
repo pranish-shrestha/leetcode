e73c71f4e84a2f72023c21f3eaae5a0067ce0668
spranish45@gmail.com <spranish45@gmail.com>
2022-10-25 22:44:58 +0545

    single_number

---
+++ 136.py
@@ -0,0 +1,16 @@
+'''
+136. Single Number
+https://leetcode.com/problems/single-number/
+'''
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        s = set()
+
+        for num in nums:
+            if num in s:
+                s.remove(num)
+            else:
+                s.add(num)
+        return s.pop()
