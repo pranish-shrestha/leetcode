23243e88a75f692cfe2edb1c19b6b0d4db81c525
spranish45@gmail.com <spranish45@gmail.com>
2022-10-25 07:14:25 +0545

    length_of_last_word

---
+++ 58.py
@@ -0,0 +1,7 @@
+'''
+58. Length of Last Word
+https://leetcode.com/problems/length-of-last-word/
+'''
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        return len((s.split())[-1])
