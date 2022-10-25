b6c5b13cd822c689c3175c99cd6d3b3af26ae7a4
spranish45@gmail.com <spranish45@gmail.com>
2022-10-25 07:38:13 +0545

    search_insert_position

---
+++ 35.py
@@ -0,0 +1,14 @@
+'''
+35. Search Insert Position
+https://leetcode.com/problems/search-insert-position/
+'''
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+            if nums[i] > target:
+                return i
+        return len(nums)
