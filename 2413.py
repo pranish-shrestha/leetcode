cef104bdf49421758409e68d8863eb9317ed7c0d
spranish45@gmail.com <spranish45@gmail.com>
2022-10-28 20:03:32 +0545

    smallest_even_multiple

---
+++ 2413.py
@@ -0,0 +1,9 @@
+'''
+2413. Smallest Even Multiple
+https://leetcode.com/problems/smallest-even-multiple/
+'''
+
+
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        return 2*n if n % 2 else n
