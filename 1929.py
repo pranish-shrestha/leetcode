275dc536e92c69b4432d23d1da11c8636cab3503
spranish45@gmail.com <spranish45@gmail.com>
2022-10-28 19:38:50 +0545

    concatenation_of_array

---
+++ 1929.py
@@ -0,0 +1,9 @@
+'''
+1929. Concatenation of Array
+https://leetcode.com/problems/concatenation-of-array/
+'''
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums*2
